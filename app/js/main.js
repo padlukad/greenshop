0b476d52d0752f47f9421f2dc2b1df4ee3de4790
Ihor <72511454+padlukad@users.noreply.github.com>
2022-03-22 20:14:56 +0200

    added fonts, header, footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function() {
     
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: true,
+        initialSlide: 1,
+        fade: true,
+    });
+
 });
