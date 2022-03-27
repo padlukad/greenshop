738a37856928b9662ded9cecd6399c3514e3df09
Ihor <72511454+padlukad@users.noreply.github.com>
2022-03-27 23:39:42 +0300

    added login popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,39 @@
 $(function() {
 
+   
+    $(function(){
+        const openModalButtons = document.querySelectorAll('[data-modal-target]')
+        const closeModalButtons = document.querySelectorAll('[data-close-button]')
+        const overlay = document.getElementById('overlay')
+
+        openModalButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const modal = document.querySelector(button.dataset.modalTarget)
+                openModal(modal)
+            })
+        })
+
+        closeModalButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const modal = button.closest('.modal')
+                closeModal(modal)
+            })
+        })
+
+        function openModal(modal) {
+            if (modal == null) return
+            modal.classList.add('active')
+            overlay.classList.add('active')
+        }
+
+        function closeModal(modal) {
+            if (modal == null) return
+            modal.classList.remove('active')
+            overlay.classList.remove('active')
+        }
+    });
+
+
     $(function() {
         const UPLOAD_BUTTON = document.getElementById("upload-button");
         const FILE_INPUT = document.querySelector("input[type=file]");
